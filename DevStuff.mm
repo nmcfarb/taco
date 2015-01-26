e08b85aebb076c63c95a9078943725909bd64a90
nmcfarb <nmcfarb@gmail.com>
2015-01-26 18:30:28 -0500

    adding devstuff.mm

---
+++ DevStuff.mm
@@ -0,0 +1,113 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Dev Stuff" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1421350305785"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="GitHub" POSITION="right" ID="ID_447283743" CREATED="1421350317219" MODIFIED="1421350673587" HGAP="-190" VSHIFT="-70" LINK="https://github.com/nmcfarb/taco-verde#taco-verde">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="MEAN stack" POSITION="left" ID="ID_550445475" CREATED="1421350981277" MODIFIED="1421350988896">
+<edge COLOR="#0000ff"/>
+<node TEXT="tutorial" ID="ID_686679578" CREATED="1421350998727" MODIFIED="1421351053854" LINK="https://thinkster.io/angulartutorial/mean-stack-tutorial/" HGAP="60" VSHIFT="-50"/>
+<node TEXT="start up and cfg" ID="ID_1117349466" CREATED="1421351071634" MODIFIED="1421940911912" HGAP="80" VSHIFT="-20"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      start&#160;&#160;node
+    </p>
+    <p>
+      browse to folder with package.json
+    </p>
+    <p>
+      cli-
+    </p>
+    <p>
+      $npm start
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      go to
+    </p>
+    <p>
+      http://localhost:3000
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      /home/dad/projects/express/taco-verde/views/main.js
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="issues" ID="ID_57392710" CREATED="1421352859084" MODIFIED="1421352874210"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Warning: missing space before text for line 6 of jade file
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
